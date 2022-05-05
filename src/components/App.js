fd7bd3b32512000e5a9c4e9d35ac203500798447
ElenaPalomar <elenapalomarllorente@gmail.com>
2022-05-05 20:46:33 +0200

    add render API data

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,42 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
 import getApiData from '../services/fetchMoviesApi';
+// Importamos nuestro componente MovieSceneList para poder utilizarlo en App
+import MovieSceneList from './MovieSceneList';
+
 import '../styles/App.scss';
 
 function App() {
 
-  useEffect (()=> {
-    getApiData().then(movies => {
-      console.log(movies);
+  // STATES
+  // State for API data
+  const [movies, setMovies] = useState([]); 
+
+
+  // useEffect to get API data
+  useEffect (() => {
+    getApiData().then(dataClean => { 
+      setMovies(dataClean);
     });
   }, []);
 
+  
+
 
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <>
+    {/* Este es un HTML mínimo para pintar la página con un 'header' con su 'título' */}
+      <header className='header'>
+        <h1 className='header__title'>Owen Wilson's 
+        <span className="uppercase">"wow"</span>
+        </h1>
+      </header>
+
+      <main className="main">
+        {/* Aquí llamamos al componente MovieSceneList porque es donde queremos que se pinte nuestra lista de películas */}
+        <MovieSceneList movies={movies}/> {/* Le pasamos por 'props' el arrays de objetos que obtenemos de la API y está almacenado en nuestra variable de estado 'movies' para que luego MovieSceneList pueda pintar esos datos. Creamos una props llamada 'movies' y le asignamos el valor de la variable de estado {movies} */}
+      </main>
+    </>
   );
 }
 
---
+++ src/components/MovieSceneItem.js
@@ -0,0 +1,13 @@
+const MovieSceneItem = () => {
+  return (
+    // El HTML que tenemos aquí es lo que iría dentro de cada <li> de MovieSceneItem y los elementos que nos han pedido pintar en el enunciado
+    <a href="/a rellenar" className='movie__link' title='a rellenar' target='_blanck'>
+      <img className='movie__poster' alt='' />
+      <h4 className='movie__title'>A rellenar</h4>
+      <p className='movie__fullLine'></p>
+      <p className='movie__year'></p>
+    </a>
+  );
+};
+
+export default MovieSceneItem;
